33ddc0bd4dc98d82e6d922811c9159e2538d6b8a
Dendi13 <98680907+Dendi13@users.noreply.github.com>
2022-11-25 17:30:45 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
 //1
 let arr = [1, 2, 3]
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
+for(let key of arr){
+    console.log(key)
+}
 //2
 let arr2 = [1, 2, 3]
-let sum = arr2[0] + arr2[1] + arr2[2];
-console.log(sum);
+let sum = 0;
+arr2.map((item) => sum += item);
+console.log(sum); 
 //3
 let arr3 = [1, 2, 3]
 arr3.push(4, 5)
 console.log(arr3);
 //4
-let names = prompt('Как ваз зовут?');
-let userNames = prompt('Какая у вас фамилия?');
-let olds = prompt('Сколько вам лет?');
+let box = prompt('Введите ваше имя, фамилию и возраст без запятых');
+let array = box.split(' ');
 
-let box = names.split(' ');
-let [name] = box;
-
-let box2 = userNames.split(' ');
-let [userName] = box2;
-
-let box3 = olds.split(' ');
-let [old] = box3;
+let [userName, name, old] = array;
 console.log(name);
 console.log(userName);
 console.log(old);
